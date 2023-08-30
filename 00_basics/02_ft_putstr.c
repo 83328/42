fb352394f87cbd8b356fd3f2d17dcc8fbc50fbb1
Kekesi, Attila  IT/HZA-TF <KEKESATI@schaeffler.com>
2023-08-31 01:21:36 +0200

    00_basics: added 0*_putstr.c

---
+++ 00_basics/02_ft_putstr.c
@@ -0,0 +1,33 @@
+/*
+Put String
+
+Write a function that prints the given string followed by a newline.
+Make a main function to call/test this function.
+Your project must be written in accordance with the Norm of 42 (except for the 42 header).
+
+Function:
+	name:		ft_putstr
+	arguments:	char *str
+	returns:	void
+
+Allowed external functions:
+	write from <unistd.h>
+
+Compile:
+	cc -Wall -Wextra -Werror 02_ft_putstr.c -o ft_putstr
+
+Run:
+	./ft_putstr
+
+Tests:
+	ft_putstr("Hello, World!")
+	ft_putstr("42")
+	ft_putstr("")
+	ft_putstr(" ")
+	ft_putstr("NULL")
+	ft_putstr("char *str")
+	ft_putstr("\n")
+	...
+*/
+
+<YOUR CODE HERE>
---
+++ 00_basics/03_ft_putstr.c
@@ -0,0 +1,42 @@
+/*
+Put String
+
+Write a function that prints the given string followed by a newline.
+Make a main function that can get argument(s).
+If there is only one argument, call the function with it.
+In other case simply print a newline.
+Your project must be written in accordance with the Norm of 42 (except for the 42 header).
+
+Function:
+	name:		ft_putstr
+	arguments:	char *str
+	returns:	void
+
+Allowed external functions:
+	write from <unistd.h>
+
+Compile:
+	cc -Wall -Wextra -Werror 03_ft_putstr.c -o ft_putstr
+
+Run:
+	./ft_putstr
+
+Tests:
+	./ft_putstr "Hello, World!"
+	./ft_putstr 42
+	./ft_putstr 42 -123454321
+	./ft_putstr
+	./ft_putstr ""
+	./ft_putstr "" ""
+	./ft_putstr ''
+	./ft_putstr '' ''
+	./ft_putstr "" "" ''
+	./ft_putstr " "
+	./ft_putstr " " " "
+	./ft_putstr "NULL"
+	./ft_putstr "char *str"
+	./ft_putstr "\n"
+	...
+*/
+
+<YOUR CODE HERE>
