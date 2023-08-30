e3136619b2081f17f16b4a2167bb27319ee6a48c
akekesi <kekesi.att@gmail.com>
2023-08-30 18:38:42 +0200

    00_basics: added hello_world.c

---
+++ 00_basics/00_hello_world_solution.c
@@ -0,0 +1,23 @@
+/*
+Hello, World!
+
+Write a program that prints "Hello, World!" followed by a newline.
+Your project must be written in accordance with the Norm of 42 (except for the 42 header).
+
+Allowed functions:
+	write from <unistd.h>
+
+Compile:
+	cc -Wall -Wextra -Werror 00_hello_world.c -o hello_world
+
+Run:
+	./hello_world
+*/
+
+#include <unistd.h>
+
+int	main(void)
+{
+	write (1, "Hello, World!\n", 14);
+	return (0);
+}
