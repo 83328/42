578d845f91c758ce348c6724c64646a181068ea3
Arthur Limpens <arthur.limpens@gmail.com>
2023-09-15 11:31:56 +0200

    Small exam changes

--- exam_02/1_repeat_alpha.c
+++ exam_02/1_repeat_alpha.c
@@ -59,3 +59,38 @@ int	main(int ac, char **av)
 	write(1, "\n", 1);
 	return (0);
 }
+
+#include<unistd.h>
+int main(int ac, char **av)
+{
+int i = 0;
+int j = 0;
+if(ac == 2)
+{
+	while(av[1][i])
+		{
+		if(av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+			j = av[1][i] - 'a'+1;
+			while(j >= 1)
+				{
+				write(1, &av[1][i], 1);
+				j--;
+				}
+			}
+		else if(av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+			j = av[1][i] - 'A'+1;
+			while(j >= 1)
+				{
+				write(1, &av[1][i], 1);
+				j--;
+				}
+			}
+		else
+			write(1, &av[1][i], 1);
+		i++;
+		}
+}
+write(1, "\n", 1);
+}
--- exam_02/2_camel_to_snake.c
+++ exam_02/2_camel_to_snake.c
@@ -73,3 +73,31 @@ int main(int argc, char **argv)
 	}
 	write (1, "\n", 1);
 }
+
+#include <unistd.h>
+int main(int ac, char **av)
+{
+int i = 0;
+if(ac == 2)
+{
+	while(av[1][i])
+		{
+		if((av[1][i] >= 'A' && av[1][i] <= 'Z') && av[1][i-1] != '\0')
+			{
+			av[1][i] = av[1][i] +32;
+			write(1, "_", 1);
+			write(1, &av[1][i], 1);
+			}
+		else if ((av[1][i] >= 'A' && av[1][i] <= 'Z') && av[1][i-1] == '\0')
+			{
+			av[1][i] = av[1][i] +32;
+			write(1, &av[1][i], 1);
+			}
+		else
+			write(1, &av[1][i], 1);
+		i++;
+		
+		}
+}
+write(1, "\n", 1);
+}
