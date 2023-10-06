8084d5f16d1321007dda5c3c9c90977828367fdb
Arthur Limpens <arthur.limpens@gmail.com>
2023-10-06 11:44:19 +0200

    ft_range updated

--- exams/exam_02/3_ft_range.c
+++ exams/exam_02/3_ft_range.c
@@ -39,36 +39,35 @@ int	*ft_range(int start, int end)
 #include <stdlib.h>
 #include <stdio.h>
 
-int     absolute_value(int n)
+int absolute_value(int n)
 {
-    if (n < 0)
-        return (-n);
-    return (n);
+if(n < 0)
+	return(-n);
+return(n);
 }
- 
+
 int     *ft_range(int start, int end)
 {
-    int number_of_ints;
-    int *array;
-    int step;
-    int i;
- 
-    number_of_ints = 1 + absolute_value(end - start);
-    array = malloc(sizeof(int) * number_of_ints);
- 
-    if (start < end)
-        step = 1;
-    else
-        step = -1;
- 
-    i = 0;
-    while (i < number_of_ints)
-    {
-        array[i] = start;
-        start = start + step;
-        ++i;
-    }
-    return (array);
+int i = 0;
+int number_of_ints;
+int *array;
+int step;
+
+number_of_ints = 1 + absolute_value(end - start);
+array = malloc(sizeof(int) * number_of_ints);
+
+if(start < end)
+	step = 1;
+else
+	step = -1;
+
+while(i < number_of_ints)
+	{
+	array[i] = start;
+	start = start + step;
+	i++;
+	}
+return(array);
 }
 
 int		main(void)
