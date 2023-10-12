b8c8b03a0c3abf74e0ea3b8136b8add19c2c8d7b
Arthur Limpens <arthur.limpens@gmail.com>
2023-10-12 14:42:06 +0200

    Updated print_hex

--- exams/exam_02/3_print_hex.c
+++ exams/exam_02/3_print_hex.c
@@ -19,45 +19,46 @@ $> ./print_hex "5156454" | cat -e
 $> ./print_hex | cat -e
 $ */
 
+#include <unistd.h>
+
 int ft_atoi(char *str)
 {
-	int result;
-	int sign;
-	int i;
+int i = 0;
+int sign = 1;
+int res = 0;
 
-	result = 0;
-	sign = 1;
-	i = 0;
-	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if(str[i] == '-')
-		sign = -1;
-	if(str[i] == '-' || str[i] == '+')
-		i++;
-	while(str[i] >= '0' && str[i] <= '9')
+while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	i++;
+if(str[i] == '-')
+	sign = -1;
+if(str[i] == '-' || str[i] == '+')
+	i++;
+while(str[i] >= '0' && str[i] <= '9')
 	{
-		result = result*10 + str[i] - '0';
-		i++;
+	res = res * 10 + str[i] - '0';
+	i++;
 	}
-	return(sign * result);
+return(sign * res);
 }
 
-void	print_hex(int n)
+void print_hex(int n)
 {
-	char hex_digits[] = "0123456789abcdef";
-
-	if (n >= 16)
-		print_hex(n / 16);
-	write(1, &hex_digits[n % 16], 1);
+char hex_digits[] ="0123456789abcdef";
+if(n >= 16)
+	print_hex(n/16);
+write(1, &hex_digits[n%16], 1);
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac == 2)
-		print_hex(ft_atoi(av[1]));
-	write(1, "\n", 1);
-}
+int i = 0;
 
+if(ac == 2)
+{
+	print_hex(ft_atoi(av[1]));
+}
+write(1, "\n", 1);
+}
 
 /*
 #include <unistd.h>
