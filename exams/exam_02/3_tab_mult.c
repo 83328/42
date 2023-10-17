09ea7557625848c44d3fe8722a357d89c90fdd64
Arthur Limpens <arthur.limpens@gmail.com>
2023-10-17 14:11:31 +0200

    twee

--- exams/exam_02/3_tab_mult.c
+++ exams/exam_02/3_tab_mult.c
@@ -37,6 +37,64 @@ $>./tab_mult | cat -e
 $
 $> */
 
+#include<unistd.h>
+
+int ft_atoi(char *str)
+{
+int i = 0;
+int sign = 1;
+int res = 0;
+
+while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	i++;
+if(str[i] == '-')
+	sign = -1;
+if(str[i] == '-' || str[i] == '+')
+	i++;
+while(str[i] >= '0' && str[i] <= '9')
+	{
+	res = res * 10 + str[i] - '0';
+	i++;
+	}
+return(sign * res);
+}
+
+void ft_putchar(char c)
+{
+write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+if(n > 9)
+	ft_putnbr(n/10);
+ft_putchar(n % 10 + '0');
+}
+
+int main(int ac, char **av)
+{
+int i = 1;
+int flag = 0;
+int n;
+int res;
+if(ac == 2)
+	{
+	n = ft_atoi(av[1]);
+	while(i < 10)
+		{
+		ft_putnbr(i);
+		write(1, " x ", 3);
+		ft_putnbr(n);
+		write(1, " = ", 3);
+		ft_putnbr(i*n);
+		write(1, "\n", 1);
+		i++;
+		}
+	}
+else
+	write(1, "\n", 1);
+}
+
 /* #include <unistd.h>
 
 int	ft_atoi(char *str)
