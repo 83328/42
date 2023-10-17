6e1563ba08d4bed7d3a76514b34132060e39f549
Arthur Limpens <arthur.limpens@gmail.com>
2023-10-17 14:09:20 +0200

    strcmp update

--- exams/exam_02/2_ft_strcmp.c
+++ exams/exam_02/2_ft_strcmp.c
@@ -19,6 +19,17 @@ int		ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+/* 
+int main(int ac, char **av)
+{
+int i = ft_strcmp(av[1], av[2]);
+int j = 0;
+j = strcmp(av[1], av[2]);
+printf("My function: %i\n", i);
+printf("Built-in: %i\n", j);
+}
+*/
+
 /* -----------------------------------------------------------
 
 int    ft_strcmp(char *s1, char *s2)
