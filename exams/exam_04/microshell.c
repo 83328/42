18ce468a36872855f7a5b7b2530750435e4a3ca2
Arthur Limpens <arthur.limpens@gmail.com>
2024-05-02 15:41:39 +0200

    Updated exam 04

--- exams/exam_04/microshell.c
+++ exams/exam_04/microshell.c
@@ -1,67 +1,63 @@
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "microshell.h"	
 
-int	err(char *str)
+int err(char *str)
 {
-	while (*str)
+	while(*str)
 		write(2, str++, 1);
 	return 1;
 }
 
-int	cd(char **av, int i)
+int cd(char **argv, int i)
 {
-	if (i != 2)
+	if(i != 2)
 		return err("error: cd: bad arguments\n");
-	if (chdir(av[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(av[1]), err("\n");
+	else if(chdir(argv[1]) == -1)
+		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
 	return 0;
 }
 
-int	exec(char **av, char **envp, int i)
+int exec(char **argv, char **envp, int i)
 {
-	int	fd[2];
+	int fd[2];
 	int status;
-	int	has_pipe = av[i] && !strcmp(av[i], "|");
-
-	if (has_pipe && pipe(fd) == -1)
-		return err("error: fatal\n");
+	int has_pipe = argv[i] && !strcmp(argv[i], "|");
 	
-	int	pid = fork();
-	if (!pid)
-	{
-		av[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-			return err("error: fatal\n");
-		execve(*av, av, envp);
-		return err("error: cannot execute "), err(*av), err("\n");
-	}
-
-	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+	if(has_pipe && pipe(fd) == -1)
 		return err("error: fatal\n");
-
+	int pid = fork();
+	if(!pid)
+	{
+		argv[i] = 0;
+		if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return err("error: fatal\n");
+		execve(*argv, argv, envp);
+		return err("error: cannot execute "), err(*argv), err("\n");
+	}
+	waitpid(pid, &status, 0);
+	if(has_pipe && (dup2(fd[0], 0) == -1 || close(fd[1]) == -1 || close(fd[0]) == -1))
+		return err("error: fatal\n");
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int	main(int ac, char **av, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	int	i = 0;
+	int i = 0;
 	int status = 0;
-
-	if (ac > 1)
+	
+	if(argc > 1)
 	{
-		while (av[i] && av[++i])
+		while(argv[i] && argv[++i])
 		{
-			av += i;
+			argv += i;
 			i = 0;
-			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+			while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
 				i++;
-			if (!strcmp(*av, "cd"))
-				status = cd(av, i);
+			if(!strcmp(*argv, "cd"))
+				status = cd(argv, i);
 			else if(i)
-				status = exec(av, envp, i);
+				status = exec(argv, envp, i);
 		}
 	}
 	return status;
 }
+			
