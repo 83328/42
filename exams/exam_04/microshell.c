4e0ba6d6a7c6085470989042468ebde9dffc2012
Arthur Limpens <arthur.limpens@gmail.com>
2024-06-10 17:24:07 +0200

    updated exam 04

--- exams/exam_04/microshell.c
+++ exams/exam_04/microshell.c
@@ -1,4 +1,4 @@
-#include "microshell.h"	
+#include "microshell.h"
 
 int err(char *str)
 {
@@ -7,31 +7,31 @@ int err(char *str)
 	return 1;
 }
 
-int cd(char **argv, int i)
+int cd(char **av, int i)
 {
 	if(i != 2)
 		return err("error: cd: bad arguments\n");
-	else if(chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
+	else if(chdir(av[1]) == -1)
+		return err("error: cd: cannot change directory to "), err(av[1]), err("\n");
 	return 0;
 }
 
-int exec(char **argv, char **envp, int i)
+int exec(char **av, char **envp, int i)
 {
 	int fd[2];
 	int status;
-	int has_pipe = argv[i] && !strcmp(argv[i], "|");
-	
+	int has_pipe = av[i] && !strcmp(av[i], "|");
+
 	if(has_pipe && pipe(fd) == -1)
 		return err("error: fatal\n");
 	int pid = fork();
 	if(!pid)
 	{
-		argv[i] = 0;
+		av[i] = 0;
 		if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return err("error: fatal\n");
-		execve(*argv, argv, envp);
-		return err("error: cannot execute "), err(*argv), err("\n");
+		execve(*av, av, envp);
+		return err("error: cannot execute "), err(*av), err("\n");
 	}
 	waitpid(pid, &status, 0);
 	if(has_pipe && (dup2(fd[0], 0) == -1 || close(fd[1]) == -1 || close(fd[0]) == -1))
@@ -39,25 +39,24 @@ int exec(char **argv, char **envp, int i)
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int main(int argc, char **argv, char **envp)
+int main(int ac, char **av, char **envp)
 {
 	int i = 0;
 	int status = 0;
-	
-	if(argc > 1)
+
+	if(ac > 1)
 	{
-		while(argv[i] && argv[++i])
+		while(av[i] && av[++i])
 		{
-			argv += i;
+			av += i;
 			i = 0;
-			while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 				i++;
-			if(!strcmp(*argv, "cd"))
-				status = cd(argv, i);
+			if(!strcmp(*av, "cd"))
+				status = cd(av, i);
 			else if(i)
-				status = exec(argv, envp, i);
+				status = exec(av, envp, i);
 		}
 	}
 	return status;
 }
-			
